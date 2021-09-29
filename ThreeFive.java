eccd5266a3b121f0515a41c9c88206e988ceab31
Suryakiran11 <91546965+Suryakiran11@users.noreply.github.com>
2021-09-30 00:32:14 +0530

    Add files via upload

---
+++ ThreeFive.java
@@ -0,0 +1,16 @@
+package com.hcl;
+
+public class ThreeFive {
+
+	public static void main(String[] args) {
+		int count=0;
+		for(int i=0;i<1000;i++)
+		{
+			if(i%5==0||i%3==0)
+				count=count+i;
+			
+		}
+		System.out.println(count);
+	}
+
+}
